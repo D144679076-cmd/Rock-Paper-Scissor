263a8dba8c820480e1828d8c8833077d1442a9cf
Dgaming144 <duynguyen1442007@gmail.com>
2023-08-18 14:23:23 +0700

    Update index file

--- paper-rock-scissor/Data/Sign_in_data.cs
+++ paper-rock-scissor/Data/Sign_in_data.cs
@@ -4,6 +4,11 @@
     {
         static public bool Signed_in_data {get;set;} = false;
     }
+    public class Scorce
+    {
+        static public int Player1 {get;set;} =0;
+        static public int Player2 {get;set;} =0;
+    }
 }
 
 
